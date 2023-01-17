df6aefbd31ed034a50c6b2d0aa12e8749109fd99
Ddggdd135 <66861021+JWJUN233233@users.noreply.github.com>
2023-01-17 11:44:51 +0800

    Delete DLogger directory

--- DLogger/utils.h
+++
@@ -1,5 +0,0 @@
-#include <Windows.h>
-#pragma once
-void utf8ToGbk(char* utf8String, char* gbkString);
-void Char2Wchar(wchar_t* wcharStr, char* charStr);
-void Wchar2Char(wchar_t* wcharStr, char* charStr);
