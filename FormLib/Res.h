3446bcecf11c8328ee4d4a3872530ad00f2673fe
Ddggdd135 <66861021+JWJUN233233@users.noreply.github.com>
2023-01-17 11:45:23 +0800

    Delete FormLib directory

--- FormLib/Res.h
+++
@@ -1,2 +0,0 @@
-#pragma once
-#define IDI_ICON1  100
--- FormLib/class.h
+++
@@ -1,6 +0,0 @@
-#include "Point.h"
-#include "Size.h"
-#include "Form.h"
-#include "utils.h"
-using namespace DG_CoreLib;
-using namespace FormLib;
